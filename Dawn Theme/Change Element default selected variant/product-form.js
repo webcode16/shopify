ce74786aaf5bba17a6b6fd69d139dabbba5ab1b8
webcode16 <134147191+webcode16@users.noreply.github.com>
2025-08-25 13:13:24 +0530

    Update product-form.js

--- Dawn Theme/Change Element default selected variant/product-form.js
+++ Dawn Theme/Change Element default selected variant/product-form.js
@@ -164,12 +164,13 @@ if (!customElements.get('product-info')) {
       handleUpdateProductInfo(productUrl) {
         return (html) => {
           const variant = this.getSelectedVariant(html);
-          // ✅ Update variant description
+          // ✅ Start Update variant description
           const newDescription = html.getElementById(`VariantDescription-${this.dataset.section}`);
           const currentDescription = this.querySelector(`#VariantDescription-${this.dataset.section}`);
           if (newDescription && currentDescription) {
             currentDescription.innerHTML = newDescription.innerHTML;
           }
+          // End
 
           this.pickupAvailability?.update(variant);
           this.updateOptionValues(html);
@@ -420,3 +421,4 @@ if (!customElements.get('product-info')) {
     }
   );
 }
+
